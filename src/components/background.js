b9507efde7789d473d2dde1866e24cf8ba104a39
Hixmixly <152249445+Hixmixly@users.noreply.github.com>
2024-07-31 21:40:01 -0700

    added image

---
+++ src/components/background.js
@@ -0,0 +1,27 @@
+
+
+
+function background() {
+    return(
+        <div ima>
+            <div>
+                <div>
+                    <div>
+                    
+                    </div>
+                </div>
+                <div>
+                    <h1 class="e-32dch4">Order groceries for delivery or pickup today</h1>
+                        <h1 class="e-32dch4">Order groceries for delivery or pickup today</h1>
+                        <p class="e-wznpl3">Whatever you want from local stores, brought right to your door.</p>
+                </div>
+                <div>
+                    <img src="https://www.instacart.com/image-server
+                                /x428/www.instacart.com/assets/homepage
+                                /homepage_background_full_m3_cropped
+                                -8d2d286263821da7decd7c61fb1db1eb0e3dec13e0c356277d6d3cb7484c024a.jpg"/>
+                </div>
+            </div>
+        </div>
+    )
+}
