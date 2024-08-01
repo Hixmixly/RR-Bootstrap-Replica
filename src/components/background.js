fc32048ca1659a0290aeab2c5f69235a7b860e8a
Hixmixly <152249445+Hixmixly@users.noreply.github.com>
2024-07-31 21:45:54 -0700

    added listings code, will need to debug

--- src/components/background.js
+++ src/components/background.js
@@ -16,10 +16,11 @@ function background() {
                         <p class="e-wznpl3">Whatever you want from local stores, brought right to your door.</p>
                 </div>
                 <div>
-                    <img src="https://www.instacart.com/image-server
+                    <img  style = {{ position: 'absolute', backgroundSize: auto,
+                    backgroundRepeat: no-repeat, backgroundColor: #deedd6,}} src="https://www.instacart.com/image-server
                                 /x428/www.instacart.com/assets/homepage
                                 /homepage_background_full_m3_cropped
-                                -8d2d286263821da7decd7c61fb1db1eb0e3dec13e0c356277d6d3cb7484c024a.jpg"/>
+                                -8d2d286263821da7decd7c61fb1db1eb0e3dec13e0c356277d6d3cb7484c024a.jpg" alt="veg-card"/>
                 </div>
             </div>
         </div>
