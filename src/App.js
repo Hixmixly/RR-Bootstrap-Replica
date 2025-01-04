b09c9dac00bf57a2bbceb45ab1f342123697e63b
Hixmixly <152249445+Hixmixly@users.noreply.github.com>
2025-01-03 20:10:37 -0800

    had to delete TopBar due to an error with the way i originally named it, that being topBar.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
 import './App.css'
-import TopBar from './components/TopBar';
 import 'bootstrap/dist/css/bootstrap.min.css'; 
 
 function App() {
   return (
     <div>
-      <TopBar />
+     
       <div className="container mt-5">
         <h1>Welcome to Instacart!</h1>
         <p>This is a sample landing page.</p>
