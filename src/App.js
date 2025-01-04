4e8177d02f5f1f87b1108d764390c40baa114c26
Hixmixly <152249445+Hixmixly@users.noreply.github.com>
2025-01-03 18:47:58 -0800

    renamed TopBar to topBar

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
-import './App.css';
-import 'bootstrap/dist/css/bootstrap.min.css';
+import './App.css'
+import TopBar from './components/TopBar';
+import 'bootstrap/dist/css/bootstrap.min.css'; 
 
 function App() {
   return (
-    <div className="App">
-      <h1>Hello World</h1>
+    <div>
+      <TopBar />
+      <div className="container mt-5">
+        <h1>Welcome to Instacart!</h1>
+        <p>This is a sample landing page.</p>
+      </div>
     </div>
   );
 }
 
 export default App;
+
